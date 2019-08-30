e4f3407484ea569bcb10c831be577523b4de7270
zsakvo <gdwqsp@gmail.com>
2019-08-30 22:57:25 +0800

    update apis

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-var gs = require("./lib/getSearch");
-var bd = require("./lib/bookDetail");
-var gc = require("./lib/getContent");
+var search = require("./lib/search");
+var detail = require("./lib/detail");
+var content = require("./lib/content");
+var type = require("./lib/type");
+var review = require("./lib/review");
 var config = require("./config");
 
 var app = express();
@@ -18,19 +20,33 @@ app.listen(config.port);
 app.get("/book/search", async function(req, res) {
   var keyword = req.query.keyword;
   var page = req.query.page;
-  res.json(await gs(keyword, page));
+  res.json(await search(keyword, page));
   res.end();
 });
 
 app.get(/^\/book\/(\d+)\/index\.htm$/, async function(req, res) {
   var bid = req.url.split("/")[2];
-  res.json(await bd(bid));
+  res.json(await detail(bid));
   res.end();
 });
 
 app.get(/^\/book\/(\d+)\/(\d+)\.htm$/, async function(req, res) {
   var bid = req.url.split("/")[2];
   var cid = req.url.split("/")[3];
-  res.json(await gc(bid, cid));
+  res.json(await content(bid, cid));
+  res.end();
+});
+
+app.get("/book/index.php", async function(req, res) {
+  var type = req.query.type;
+  var page = req.query.page;
+  res.json(await type(type, page));
+  res.end();
+});
+
+app.get("/review/index.php", async function(req, res) {
+  var sort = req.query.type;
+  var page = req.query.page;
+  res.json(await review(sort, page));
   res.end();
 });
